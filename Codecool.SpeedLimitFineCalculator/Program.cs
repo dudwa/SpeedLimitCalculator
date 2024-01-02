27bf3484c46dcb77dac1c441693fe1b94278ada6
dudwa <dorian.dudas@gmail.com>
2024-01-02 16:16:13 +0100

    feat: Add the final Program.cs to the project

---
+++ Codecool.SpeedLimitFineCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.SpeedLimitFineCalculator.Service;
+using Codecool.SpeedLimitFineCalculator.UI;
+
+namespace Codecool.SpeedLimitFineCalculator;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        IVehicleLimitCalculator vehicleLimitCalculator = null;
+        ISpeedLimitFineCalculator speedLimitFineCalculator = null;
+        var fineCalculatorUi = new FineCalculatorUi(speedLimitFineCalculator);
+        fineCalculatorUi.Run();
+    }
+}
